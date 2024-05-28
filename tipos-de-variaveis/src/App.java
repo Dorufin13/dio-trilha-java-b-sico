33c486fa7dfbd4bc3cc17d2f0f659e71196bfe13
Dorufin13 <gabrielbarbosalopesl@gmail.com>
2024-05-28 10:39:04 -0300

    Alguns tipos de variáveis que aprendi no Java

--- tipos-de-variaveis/src/App.java
+++ tipos-de-variaveis/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte dedos = 10;
+        short numero = 349;
+        int salarioMinimo = 2500;
+        
+        String meuNome = "Gabriel";
+
+        System.out.println(salarioMinimo);
     }
 }
